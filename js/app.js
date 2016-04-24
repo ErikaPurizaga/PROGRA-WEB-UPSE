a48206c3b6612d27ffe027981cec0e7aad972463
Erika-Lily <eri.purizaga92@gmail.com>
2016-04-24 16:18:56 -0500

    AGREGANDO MENU DESPLEGABLE EN TODAS LAS PAGINAS COM JQUERY

--- js/app.js
+++ js/app.js
@@ -37,3 +37,46 @@ console.log(href);
 $overlay.click(function(){
 	$overlay.hide();
 });
+
+
+/*****************MENU PARA MOVILES*******************/
+//CREEMOS EL MENU LATERAL 
+var $select = $("<select></select>");
+$("#menu").append($select);
+
+//RECORRER MENÚ
+$("#menu a").each(function(){
+var $anchor =$(this);
+//crear una opcion para el select por cada elemnto del
+var $option = $("<option></option>");
+//obtener  cada valor de la opciin del atributo href
+$option.val ($anchor.attr("href"));
+//OBTENER EL VAL DE CADA OPCION
+$option.text ($anchor.text());
+//agregar la opcion al select
+$select.append($option);
+
+
+//OBTNER SI EL LINK ES LA PAGINA ACTUAL
+if($(this).hasClass("selected")){
+	$option.prop("selected",true);
+}
+
+
+}); 
+$select.change(function()
+{
+	window.location= $select.val();
+
+});
+
+/*var $button = $("<button>Go</button>");
+$("#menu").append($button(){
+	window.location = $select.val();
+
+});
+
+$button.click (function(){
+	//Toma el valor de la opccion seleccionada
+	window.location= $select.val();
+});*/
